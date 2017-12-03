fc9d2a34dba2441a2a0176a0cb5f6899ba8a5264
Dun Ma <madun@hotmail.com>
2017-12-03 16:58:30 -0600

    update

--- src/nimGame/Board.java
+++ src/nimGame/Board.java
@@ -6,30 +6,42 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Board {
-	
+
 	private Player playerA;
-	
+
 	private Player playerB;
-	
+
 	private List<Integer> noOfStone;
-	
+
 	private int numberOfPiles;
-	
+
 	private int numberOfRounds;
-	
-	public void setBoard(int size) {
-		noOfStone = new ArrayList<>();
-		numberOfPiles = size;	
+
+	public void setBoard() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("How many piles?");
+		noOfStone = new ArrayList<>(scanner.nextInt());
+		//TODO:Random number of stones
+		for(int index = 0; index < noOfStone.size(); index++) {
+			System.out.print("How many stones for pile" + index);
+			noOfStone = new ArrayList<>(scanner.nextInt());
+		}
+		scanner.close();
 	}
-	
-	public void setNameAndAI(String newName1, boolean isAI1, 
-			String newName2, boolean isAI2) {
-		playerA.setName(newName1);
-		playerB.setName(newName2);
-		playerA.setAI(isAI1);
-		playerB.setAI(isAI2);		
+
+	public void setNameAndAI() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("What is the name for the first player? He will go first.");
+		playerA.setName(scanner.nextLine());
+		System.out.print("Is he an AI?");
+		playerA.setAI(scanner.nextBoolean());
+		System.out.print("What is the name for the second player? He will go second.");
+		playerB.setName(scanner.nextLine());
+		System.out.print("Is he an AI?");
+		playerB.setAI(scanner.nextBoolean());
+		scanner.close();
 	}
-	
+
 	public void displayBoard() {
 		int cur = numberOfRounds % 2;
 		if (cur == 0) {
@@ -39,7 +51,7 @@ public class Board {
 		}
 		System.out.println(Arrays.toString(noOfStone.toArray()));
 	}
-	
+
 	public void move() {
 		int pileIndex = -1;
 		Scanner myScan;
@@ -48,6 +60,7 @@ public class Board {
 			System.out.print("Please choose the stone piles(0 to " + (numberOfPiles - 1) + ")");
 			myScan = new Scanner(System.in);
 			pileIndex =  myScan.nextInt();
+			myScan.close();
 		}
 		System.out.print("Successfully choose pile index:" + pileIndex);
 		int stonePicked = -1;
@@ -56,7 +69,8 @@ public class Board {
 			System.out.print("Plear choose the number of stones "
 					+ "that you can going to pick up(1 + " + noOfStone.get(pileIndex) + ")");
 			myScan = new Scanner(System.in);
-			stonePicked = myScan.nextInt();	
+			stonePicked = myScan.nextInt();
+			myScan.close();
 		}
 		System.out.print("Successfully choose stone picked:" +stonePicked);
 	}
@@ -75,19 +89,18 @@ public class Board {
 				aIMove();
 			} else {
 				move();
-			}	
+			}
 		}
-		
-		
 		numberOfRounds++;
 	}
 
-	private int[] aIMove() {
+	private void aIMove() {
 		int nimSum = nimSum(noOfStone);
 		if (nimSum == 0) {
-			int a = (int) Math.random() * noOfStone.size(); 
-			int b = (int) Math.random() * noOfStone.get(a) + 1;
-			return new int[] {a,b};
+			int a = (int) Math.random() * noOfStone.size();
+			int b = (int) Math.random() * noOfStone.get(a);
+			noOfStone.set(a, b);
+			removeZeros(a);
 			//TODO:better random
 		}
 		//TODO: difficulty level
@@ -95,12 +108,13 @@ public class Board {
 		for (int index = 0; index < noOfStone.size(); index++) {
 			int n  = noOfStone.get(index);
 			if ((n & (1 << (firstZero - 1))) != 0) {
-				 return new int[] {index, n ^ nimSum};
+				noOfStone.set(index, n ^ nimSum);
+				removeZeros(index);
+				return;
 			}
 		}
-		return new int[]{0,0};
 	}
-	
+
 	private int nimSum(List<Integer> noOfStone) {
 		int sum = 0;
 		for (int n : noOfStone) {
@@ -110,21 +124,22 @@ public class Board {
 	}
 	private boolean removeZeros(int pIndex) {
 		//first check if pIndex is valid
-		if (pIndex < noOfStone.size() && noOfStone.get(pIndex) == 0) { 
+		if (pIndex < noOfStone.size() && noOfStone.get(pIndex) == 0) {
 			noOfStone.remove(pIndex);
 			return true;
 		}
 		return false;
 	}
-	
+
 	private boolean ifWin() {
 		while (removeZeros(0)) {} // May delete this
 		return noOfStone.isEmpty();
 	}
-	
+
 	public static void main(final String[] unused) {
-		setBoard();
-		setNameAndAI();
-		play();
+		Board newboard = new Board();
+		newboard.setBoard();
+		newboard.setNameAndAI();
+		newboard.play();
 	}
 }
