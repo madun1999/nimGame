981e0518c6d5bb22ecdf33a6390c2560463a9d33
Dun Ma <madun@hotmail.com>
2017-12-03 15:36:25 -0600

    update Player.java

--- src/nimGame/Player.java
+++ src/nimGame/Player.java
@@ -1,5 +1,47 @@
 package nimGame;
 
 public class Player {
-
+	/**
+	 * if the player is an AI
+	 */
+	private boolean isAI = false;
+	
+	/**
+	 * The name of the Player
+	 */
+	private String name = "Player";
+	
+	/**
+	 * @return the isAI
+	 */
+	public boolean isAI() {
+		return isAI;
+	}
+	/**
+	 * @param isAI the isAI to set
+	 */
+	public void setAI(boolean isAI) {
+		this.isAI = isAI;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		if (name == null || name == "") {
+			if (isAI) {
+				this.name = "AI Player";
+			} else {
+				this.name = "Human Player";
+			}
+		}
+		
+		this.name = name;
+	}
+	
 }
